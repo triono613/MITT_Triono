543339461f87dfb62220f6997c146f26752bfc31
triono <triono.triono1@gmail.com>
2022-07-26 19:05:19 +0700

    delete yg salah

--- routes/api.php
+++ routes/api.php
@@ -5,16 +5,10 @@ Route::group(['middleware' => ['api']], function(){
     Route::post('usersProfile/register','AuthController@register' );
     Route::post('users/login','AuthController@signin' );
 
-    Route::get('/tutorial','TutorialController@index');
-    Route::get('/tutorial/{id}','TutorialController@show');
 
 
     Route::group(['middleware' => ['jwt.auth']], function () {
 
-        Route::get('/profile', 'UserController@show') ;
-        Route::post('/tutorial', 'TutorialController@store') ;
-
-        Route::post('/tutorial_new', 'TutorialController@store') ;
 
 
 
